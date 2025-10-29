1688766ca0746ab09fb78cf5f3dc93800783a57c
Zhitkov Nikita <zhitkov.ns@gmail.com>
2025-10-29 20:50:54 +0300

    Add greeting

---
+++ Greeting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+    
+        String name = scanner.nextLine();
+        
+        System.out.println("Привет, " + name + "!");
+        
+        scanner.close();
+    }
+}
