519abee6172f98f2d421787aa27569e219c1a601
Zhitkov Nikita <zhitkov.ns@gmail.com>
2025-11-05 18:18:01 +0300

    #

---
+++ second/MyName.java
@@ -0,0 +1,7 @@
+package second;
+public class MyName {
+    public static void main(String[] args) {
+        String name = "\u041D\u0438\u043A\u0438\u0442\u0430"; // Никита
+        System.out.print("Мое имя: " + name);
+    }
+}
